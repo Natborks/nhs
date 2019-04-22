e94c08c5dacdd0ecae530dae7fcf79481c808444
natborks <natborks@gmail.com>
2019-04-22 12:20:36 -0400

    basic UI done

---
+++ application.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
+
+@app.route("/upload")
+def upload():
+	return render_template("upload.html")
